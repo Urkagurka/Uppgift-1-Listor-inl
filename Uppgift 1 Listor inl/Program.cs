4ed1e130027a8a8263d16ed04c7edf284cb07fb9
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-02-05 19:28:49 +0100

    Add project files.

---
+++ Uppgift 1 Listor inl/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift_1_Listor_inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namnen = new List<string>();
+
+            while (true)
+            {
+                string namn = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(namn))
+                {
+                    break;
+                }
+                namnen.Add(namn);
+            }
+
+            namnen.Sort();
+            namnen.Reverse();
+
+            foreach (string namn in namnen)
+            {
+                Console.WriteLine(namn);
+
+            }
+        }
+    }
+}
